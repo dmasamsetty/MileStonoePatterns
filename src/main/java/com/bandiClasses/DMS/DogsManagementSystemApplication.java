94ac9f3ec4077fa551dc27fe11c8c3c52e67ee3c
dmasamsetty <S567354@nwmissouri.edu>
2024-04-19 12:10:32 -0500

    added

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.bandiClasses.DMS;
+
+
+/**
+ * @author Dimple Masamsetty Suresh
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
